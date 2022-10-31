7eb9b7745f2a8e256b2b754f7fea8508d2babce2
hroozt <hroozt@mail.ru>
2022-10-31 11:28:27 +0700

    eee

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,8 +4,8 @@ terraform {
     bucket     = "hrooztbucket"
     region     = "ru-central1"
     key        = "network/terraform.tfstate"
-    access_key = " ИДентификатор токена сервисной учетной записи"
-    secret_key = " токен "
+    access_key = "YCAJEUmTcgjhqmkJx6guKcftY"
+    secret_key = "YCN9hIkTVByCnsbSzpHNOqSoeCrqz1d5CtCiOR__"
     skip_region_validation      = true
     skip_credentials_validation = true
   }
